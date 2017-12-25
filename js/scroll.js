0298286a68dbda7d0a02875a9156f3e090e6e8aa
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2017-12-25 18:36:53 -0200

    Added change color when over team

--- js/scroll.js
+++ js/scroll.js
@@ -32,16 +32,23 @@ $(document).ready(function (){
     $(window).scroll(()=>{
         var nav = $(".navbar");
         var a = $("a");
-        if($(document).scrollTop() >= $("#team").position().top){
+        var navbar_toggler = $(".navbar-toggler");
+        var i = $(".fa");
+        
+        if(($(document).scrollTop() + 30) >= $("#team").position().top){
             console.log($("#team").position().top);
             console.log($(document).scrollTop());
             nav.css("background","linear-gradient(#6F0101, #AA1111)");
             nav.css("color","white");
             a.css("color", "white");
+            i.css("color", "white");
+            navbar_toggler.css("border-color", "white");
         }else{
             nav.css("background","#f8f9fa");
             nav.css("color","black");
             a.css("color", "black");
+            i.css("color", "rgba(0, 0, 0, 0.5)");
+            navbar_toggler.css("border-color", "rgba(0, 0, 0, 0.1)");
         }
     });
 });
