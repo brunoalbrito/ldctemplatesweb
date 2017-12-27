f6028a04aad26690bb7683c10780822bee55db44
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2017-12-27 13:37:21 -0200

    Ajustando home

--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,26 @@
-$(document).ready(function (){
-    $("#a-header").click(function (){
+$(document).ready(function () {
+    $("#a-header").click(function () {
         $('html, body').animate({
             scrollTop: $("#header").offset().top
         }, 2000);
     });
 
-    $("#a-team").click(function (){
+    $("#a-team").click(function () {
         $('html, body').animate({
             scrollTop: $("#team").offset().top
         }, 2000);
     });
 
 
-    $("#a-contact").click(function (){
+    $("#a-contact").click(function () {
         $('html, body').animate({
             scrollTop: $("#contact").offset().top
         }, 2000);
     });
 
-    
+
     $('#search-menu').hide();
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(document).scrollTop() > 500) {
             $('#search-menu').fadeIn('slow');
         }
@@ -29,23 +29,25 @@ $(document).ready(function (){
         }
     });
 
-    $(window).scroll(()=>{
+    $(window).scroll(() => {
         var nav = $(".navbar");
         var a = $("a");
         var navbar_toggler = $(".navbar-toggler");
         var i = $(".fa");
-        
-        if(($(document).scrollTop() + 30) >= $("#team").position().top){
-            nav.css("background","linear-gradient(#6F0101, #AA1111 , #E52222)");
-            nav.css("color","white");
+        var search = $(".fa-search")
+
+        if (($(document).scrollTop() + 30) >= $("#team").position().top) {
+            nav.css("background", "linear-gradient(#6F0101, #AA1111 , #E52222)");
+            nav.css("color", "white");
             a.css("color", "white");
             i.css("color", "white");
             navbar_toggler.css("border-color", "white");
-        }else{
-            nav.css("background","#f8f9fa");
-            nav.css("color","black");
+        } else {
+            nav.css("background", "#f8f9fa");
+            nav.css("color", "black");
             a.css("color", "black");
             i.css("color", "black");
+            search.css("color", "white");
             navbar_toggler.css("border-color", "rgba(0, 0, 0, 0.1)");
         }
     });
